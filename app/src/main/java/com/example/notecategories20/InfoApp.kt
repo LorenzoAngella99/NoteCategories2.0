5638678d3b5f2daf92f5355666e85816fa33a446
Lorenzo Angella <39377057+Dragoz99@users.noreply.github.com>
2025-01-19 11:16:15 +0100

    commit: infoapp fragment + delate button.

---
+++ app/src/main/java/com/example/notecategories20/InfoApp.kt
@@ -0,0 +1,25 @@
+package com.example.notecategories20
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InfoApp.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InfoApp : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_app, container, false)
+    }
+
+}
